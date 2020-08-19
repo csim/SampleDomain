8cd1616e17e7f34127f6c84e120a73838ba585b8
Clint Simon <me@clintsimon.com>
2020-08-19 11:36:48 -0700

    Dependency consolidation and package updates

--- src/SampleApp.Shared/SampleApp.Shared.Infrastructure/Data/CosmosDbContext.cs
+++ src/SampleApp.Shared/SampleApp.Shared.Infrastructure/Data/CosmosDbContext.cs
@@ -1,6 +1,5 @@
 ﻿namespace SampleApp.Shared.Infrastructure.Data
 {
-    using System;
     using System.Linq;
     using System.Reflection;
     using System.Threading;
@@ -11,38 +10,15 @@
 
     public class CosmosDbContext : DbContext
     {
-        public CosmosDbContext(DbContextOptions<CosmosDbContext> options) //, IMediator mediator)
-            : base(options)
+        public CosmosDbContext(DbContextOptions<CosmosDbContext> options) : base(options)
         {
-            //_mediator = mediator;
         }
-
-        //public DbSet<OrderItemRecord> OrderItemss { get; set; }
 
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
         {
-            int result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-
-            // ignore events if no dispatcher provided
-            //if (_mediator == null) return result;
-
-            // dispatch events only if save was successful
-            //var entitiesWithEvents = ChangeTracker.Entries<EntityBase>()
-            //    .Select(e => e.Entity)
-            //    .Where(e => e.Events.Any())
-            //    .ToArray();
-
-            //foreach (var entity in entitiesWithEvents)
-            //{
-            //    var events = entity.Events.ToArray();
-            //    entity.Events.Clear();
-            //    foreach (var domainEvent in events)
-            //    {
-            //        await _mediator.Publish(domainEvent).ConfigureAwait(false);
-            //    }
-            //}
-
-            return result;
+            return await base
+                .SaveChangesAsync(cancellationToken)
+                .ConfigureAwait(false);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
